038b9c2c0f0f4886a3308373cf9aac782b8e7378
Gosia Szczepańska <gosia.szczepanska1@interia.pl>
2018-09-13 08:58:33 +0200

    Generating new board

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
 const container = document.querySelector('.grid-container');
 const clear = document.querySelector('#clear');
-
-generateCells(16);
+const sizeInput = document.querySelector('#gridSize');
+const generateBtn = document.querySelector('#generate');
+sizeInput.value = 16;
+generateCells(sizeInput.value);
 
 const cells = document.querySelectorAll('.cell');
 
-clear.addEventListener('click', (e) => {
-  cells.forEach((cell) => {
-    cell.classList.remove('trace');
-  });
-});
-
-
-
-cells.forEach((cell) => {
-  cell.addEventListener('mouseover', (e) => {
-    cell.classList.add('trace');
-  });
+generateBtn.addEventListener('click', (e) => {
+  clearBoard();
+  removeCells();
+  generateCells(sizeInput.value);
 });
 
 function generateCells(gridSize) {
+  container.style.gridTemplateColumns = `repeat(${sizeInput.value}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${sizeInput.value}, 1fr)`;
   for(let i = 0; i < Math.pow(gridSize, 2); i++){
     const cell = document.createElement('div');
     cell.classList.add(`cell`);
+    cell.addEventListener('mouseover', (e) => {
+      cell.classList.add('trace');
+    });
     container.appendChild(cell);
+  }
+}
+
+function clearBoard() {
+  clear.addEventListener('click', (e) => {
+    cells.forEach((cell) => {
+      cell.classList.remove('trace');
+    });
+  });
+}
+
+function removeCells() {
+  while(container.firstChild) {
+    container.removeChild(container.firstChild);
   }
 }
