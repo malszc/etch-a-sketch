cb522d2b16182de24ff6b1016ed9764f94c78aed
Gosia Szczepańska <gosia.szczepanska1@interia.pl>
2018-09-13 08:25:05 +0200

    Mouse-over leaves trace on grid

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const container = document.querySelector('.grid-container');
 generateCells(16);
 
 const cells = document.querySelectorAll('.cell');
+cells.forEach((cell) => {
+  cell.addEventListener('mouseover', (e) => {
+    cell.classList.add('trace');
+  });
+});
 
 function generateCells(gridSize) {
   for(let i = 0; i < Math.pow(gridSize, 2); i++){
