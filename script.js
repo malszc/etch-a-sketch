e64c4a78b51b2cf307750f22a126110a394175f8
Gosia Szczepańska <gosia.szczepanska1@interia.pl>
2018-09-13 08:35:08 +0200

    Clearing board

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const container = document.querySelector('.grid-container');
+const clear = document.querySelector('#clear');
 
 generateCells(16);
 
 const cells = document.querySelectorAll('.cell');
+
+clear.addEventListener('click', (e) => {
+  cells.forEach((cell) => {
+    cell.classList.remove('trace');
+  });
+});
+
+
+
 cells.forEach((cell) => {
   cell.addEventListener('mouseover', (e) => {
     cell.classList.add('trace');
