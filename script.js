a77553081626a54e2a7268a0b206d966ac305a00
Gosia Szczepańska <gosia.szczepanska1@interia.pl>
2018-09-13 08:08:06 +0200

    Styling grid

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let gridSize = 16;
 
 for(let i = 0; i < Math.pow(gridSize, 2); i++){
   const cell = document.createElement('div');
-  cell.classList.add(`.cell`);
+  cell.classList.add(`cell`);
   container.appendChild(cell);
 }
