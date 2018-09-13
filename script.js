b22c2cb62dde19a640dc33ac92ebaa7ec720371d
Gosia Szczepańska <gosia.szczepanska1@interia.pl>
2018-09-13 07:46:44 +0200

    Generating cells

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('.grid-container');
+let gridSize = 16;
+
+for(let i = 0; i < Math.pow(gridSize, 2); i++){
+  const cell = document.createElement('div');
+  cell.classList.add(`.cell`);
+  container.appendChild(cell);
+}
