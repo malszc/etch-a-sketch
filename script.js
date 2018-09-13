0352eab14075655a1c7cf7fc86ce901bb8055cb8
Gosia Szczepańska <gosia.szczepanska1@interia.pl>
2018-09-13 08:18:12 +0200

    Refactor generating cells

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const container = document.querySelector('.grid-container');
-let gridSize = 16;
 
-for(let i = 0; i < Math.pow(gridSize, 2); i++){
-  const cell = document.createElement('div');
-  cell.classList.add(`cell`);
-  container.appendChild(cell);
+generateCells(16);
+
+const cells = document.querySelectorAll('.cell');
+
+function generateCells(gridSize) {
+  for(let i = 0; i < Math.pow(gridSize, 2); i++){
+    const cell = document.createElement('div');
+    cell.classList.add(`cell`);
+    container.appendChild(cell);
+  }
 }
